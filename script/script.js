ddff45c973a02000a016b1b93158566b359a46d4
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-02-28 01:35:39 +0530

    fix: css

--- script/script.js
+++ script/script.js
@@ -5,3 +5,15 @@ function slide() {
 	document.getElementsByClassName("curtainContainer")[1].style.transform =
 		"translatex(150vw)";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Create an audio element
+    var audio = new Audio("../assets/biotex.mp3");
+    
+    // Set attributes for autoplay and loop
+    audio.autoplay = true;
+    audio.loop = true;
+
+    // Append the audio element to the body
+    document.body.appendChild(audio);
+});
